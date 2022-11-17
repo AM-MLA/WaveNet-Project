14ad57105864e9635a35924a5454432596b9abca
Maxime Boulanger <maximeboulanger_@hotmail.fr>
2022-11-17 15:56:12 +0100

    dataset creation with 100 audios files

---
+++ model training.py
@@ -0,0 +1,52 @@
+import numpy as np
+import tensorflow as tf
+import os
+from scipy.io import wavfile
+import matplotlib.pyplot as plt
+
+from wavenet_model import WaveNet_model
+
+
+## Data packaging
+
+path = "/Users/maximeboulanger/Desktop/100 common voices  8bits/audio"
+os.chdir(path)
+
+samplerate, data = wavfile.read(os.listdir()[0])
+
+# audio files have been resampled in 8 bits and 16kHz frequency
+
+audios = np.ones((100,samplerate*10))*128
+for k in range(len(os.listdir())):
+    filename = os.listdir()[k]
+    if filename[-4:]=='.wav':
+        samplerate,data = wavfile.read(os.listdir()[k])
+        if len(data)<=samplerate*10:
+            audios[k,:len(data)] = data
+        else:
+            audios[k,:]=data[:samplerate*10]
+
+plt.figure()
+plt.plot([k/samplerate for k in range(len(audios[10,:]))],audios[10,:])
+plt.xlabel("time (s)")
+plt.ylabel("bits (/256)")
+plt.show()
+
+
+nb_train_audios = int(0.8*len(os.listdir()))
+train_audios = audios[:nb_train_audios]/255.0
+test_audios = audios[nb_train_audios:]/255.0
+
+## Train model
+
+WNmodel = WaveNet_model(
+    nb_layers = 10,
+    nb_filters = 256,
+    audio_length=samplerate*10 # length of audio files is 10s max
+).model
+
+WNmodel.summary()
+
+sgd = tf.keras.optimizers.Adam()  #SGD(lr=0.001, momentum=0.9) 
+WNmodel.compile(sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+
---
+++ wavenet_model.py
@@ -0,0 +1,79 @@
+import tensorflow as tf
+import numpy as np
+import os
+
+class WaveNet_model():
+
+    def __init__(self, nb_layers:int, nb_filters:int, audio_length:int):
+        
+        self.nb_filters= nb_filters
+
+        #à compléter
+        inputWN = tf.keras.Input(shape = (audio_length, 256), name="WN Input")
+
+        out = tf.keras.layers.Conv1D(filters=nb_filters,kernel_size=2, padding="causal", name='initial_causal_convolution')(inputWN)
+
+        skip_connections = []
+        for k in range(nb_layers):
+            out, skipx = self.__residual_block(out)
+            skip_connections.append(skipx)
+
+        out = tf.keras.layers.Add()(skip_connections)
+        out = tf.keras.layers.Activation('relu')(out)
+        out = tf.keras.layers.Conv1D(filters=256, kernel_size=1, padding = "same")(out)
+        out = tf.keras.layers.Activation('relu')(out)
+        out = tf.keras.layers.Conv1D(filters=256, kernel_size=1, padding = "same")(out)
+
+
+        outputWN =  tf.keras.layers.Activation('softmax', name="WN_Output")(out)
+
+        self.model = tf.keras.models.Model(inputs = inputWN, outputs = outputWN)
+        
+        # return WNmodel
+
+
+    def __delated_conv(self, input, nb_dilatation : int) -> tf.keras.layers.Conv1D :
+        for i in range(nb_dilatation):
+            delated_conv_f = tf.keras.layers.Conv1D(dilation_rate = 2**i) (delated_conv_f)
+
+
+    def __residual_block(self,x):
+        # x : output of the 
+        # delated conv filter
+        delated_conv_f0 = tf.keras.layers.Conv1D(filters=self.nb_filters,padding = "causal", kernel_size = 2, dilation_rate = 2**0)(x)
+        delated_conv_f1 = tf.keras.layers.Conv1D(filters=self.nb_filters,padding = "causal", kernel_size = 2,dilation_rate = 2**1)(delated_conv_f0)
+        delated_conv_f2 = tf.keras.layers.Conv1D(filters=self.nb_filters,padding = "causal", kernel_size = 2,dilation_rate = 2**2)(delated_conv_f1)
+        delated_conv_f3 = tf.keras.layers.Conv1D(filters=self.nb_filters,padding = "causal", kernel_size = 2,dilation_rate = 2**3)(delated_conv_f2)
+        delated_conv_f4 = tf.keras.layers.Conv1D(filters=self.nb_filters,padding = "causal", kernel_size = 2,dilation_rate = 2**4)(delated_conv_f3)
+        delated_conv_f5 = tf.keras.layers.Conv1D(filters=self.nb_filters,padding = "causal", kernel_size = 2,dilation_rate = 2**5)(delated_conv_f4)
+        delated_conv_f6 = tf.keras.layers.Conv1D(filters=self.nb_filters,padding = "causal", kernel_size = 2,dilation_rate = 2**6)(delated_conv_f5)
+        delated_conv_f7 = tf.keras.layers.Conv1D(filters=self.nb_filters,padding = "causal", kernel_size = 2,dilation_rate = 2**7)(delated_conv_f6)
+        delated_conv_f8 = tf.keras.layers.Conv1D(filters=self.nb_filters,padding = "causal", kernel_size = 2,dilation_rate = 2**8)(delated_conv_f7)
+        delated_conv_f9 = tf.keras.layers.Conv1D(filters=self.nb_filters,padding = "causal", kernel_size = 2,dilation_rate = 2**9)(delated_conv_f8)
+        tanh_out = tf.keras.layers.Activation('tanh')(delated_conv_f9)
+
+         # delated conv gate
+
+        delated_conv_g0 = tf.keras.layers.Conv1D(filters=self.nb_filters,padding = "causal", kernel_size = 2, dilation_rate = 2**0)(x)
+        delated_conv_g1 = tf.keras.layers.Conv1D(filters=self.nb_filters,padding = "causal", kernel_size = 2,dilation_rate = 2**1)(delated_conv_g0)
+        delated_conv_g2 = tf.keras.layers.Conv1D(filters=self.nb_filters,padding = "causal", kernel_size = 2,dilation_rate = 2**2)(delated_conv_g1)
+        delated_conv_g3 = tf.keras.layers.Conv1D(filters=self.nb_filters,padding = "causal", kernel_size = 2,dilation_rate = 2**3)(delated_conv_g2)
+        delated_conv_g4 = tf.keras.layers.Conv1D(filters=self.nb_filters,padding = "causal", kernel_size = 2,dilation_rate = 2**4)(delated_conv_g3)
+        delated_conv_g5 = tf.keras.layers.Conv1D(filters=self.nb_filters,padding = "causal", kernel_size = 2,dilation_rate = 2**5)(delated_conv_g4)
+        delated_conv_g6 = tf.keras.layers.Conv1D(filters=self.nb_filters,padding = "causal", kernel_size = 2,dilation_rate = 2**6)(delated_conv_g5)
+        delated_conv_g7 = tf.keras.layers.Conv1D(filters=self.nb_filters,padding = "causal", kernel_size = 2,dilation_rate = 2**7)(delated_conv_g6)
+        delated_conv_g8 = tf.keras.layers.Conv1D(filters=self.nb_filters,padding = "causal", kernel_size = 2,dilation_rate = 2**8)(delated_conv_g7)
+        delated_conv_g9 = tf.keras.layers.Conv1D(filters=self.nb_filters,padding = "causal", kernel_size = 2,dilation_rate = 2**9)(delated_conv_g8)
+        sigmoid_out = tf.keras.layers.Activation('sigmoid')(delated_conv_g9)
+
+        # merge x tanh & sigmoid
+        multiply = tf.keras.layers.Multiply()([tanh_out, sigmoid_out])
+
+        # conv 1,1
+        resx = tf.keras.layers.Conv1D(filters=self.nb_filters,kernel_size=1, padding='same')(multiply)
+        skipx = tf.keras.layers.Conv1D(filters=self.nb_filters,kernel_size=1, padding='same')(multiply)
+        resx = tf.keras.layers.Add()([x, resx])
+
+        return resx, skipx
+        
+    
